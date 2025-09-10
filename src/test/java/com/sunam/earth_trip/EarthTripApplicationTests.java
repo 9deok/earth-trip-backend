5218839e523f4a1f1ad8f846205a87fe34c92f00
9deok <butterflynine20@gmail.com>
2025-09-10 21:24:07 +0900

    first commit

---
+++ src/test/java/com/sunam/earth_trip/EarthTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunam.earth_trip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EarthTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
